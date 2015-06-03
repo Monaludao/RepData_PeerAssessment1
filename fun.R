dd8f909798d7ce6b90a4fd9a7f18c0d6f8f27b6a
Richard Hsieh <monaludao@gmail.com>
2015-06-03 21:39:06 +0800

    code for part 4
    code for part 4

--- fun.R
+++ fun.R
@@ -10,7 +10,10 @@ fun1 <- function(){
     
     date.step<-dcast(melt.data,date ~ "steps", sum)
     
-    hist(date.step$steps)
+    png(file="hist_date_steps.png")
+    with(date.step,hist(steps,
+                        main="The Total Number of Steps Taken Each Day"))
+    dev.off()
     
     mean.date.step<-mean(date.step$steps,na.rm=TRUE)
     med.date.step<-median(date.step$steps,na.rm=TRUE)
@@ -26,7 +29,40 @@ fun1 <- function(){
     max.interval<-interval.step[interval.step$steps == 
                                     max(interval.step[,2]),1]
     
-    plot(interval.step$interval,interval.step$steps,type="l")
+    png("plot_interval_steps.png")
+    with(interval.step,plot(interval,steps,type="l", 
+                            main="The Average Number of Steps Taken per 5-minute Interval"))
+    dev.off()
+    
     print(paste("The interval of", max.interval, 
                 "contains the max number of steps."))
+    
+    print(paste("The total number of missing values in the dataset is:",
+                nrow(subset(act_data,is.na(act_data$steps)))))
+
+    fill_data <- merge(act_data, interval.step, by="interval", 
+                     suffixes=c("", "_intervalmean"))
+    fill_data[is.na(fill_data$steps), "steps"] = 
+        fill_data[is.na(fill_data$steps), "steps_intervalmean"]
+    fill_data$steps_intervalmean <- NULL
+    ##fill_data <- fill_data[,c(2,3,1)]
+    ##fill_data <- fill_data[order(fill_data$date,fill_data$interval),]
+    ##row.names(fill_data) <- NULL
+    
+    melt.fill.data<-melt(fill_data,id=c("date","interval"),
+                         measure.vars=c("steps"))
+    date.step.f<-dcast(melt.fill.data,date ~ "steps", sum)
+    
+    png(file="hist_date_steps_filled.png")
+    with(date.step.f,hist(steps,
+                        main="The Total Number of Steps Taken Each Day"))
+    dev.off()
+    
+    mean.date.step.f<-mean(date.step.f$steps,na.rm=TRUE)
+    med.date.step.f<-median(date.step.f$steps,na.rm=TRUE)
+    
+    print(paste("The mean of the total number of steps taken per day is ",
+                mean.date.step.f, ".", sep=""))
+    print(paste("The median of the total number of steps taken per day is ",
+                med.date.step.f, ".", sep=""))
 }
